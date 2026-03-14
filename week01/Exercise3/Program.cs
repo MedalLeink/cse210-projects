2eb15a5793f84ae935a47607005794814eb90d94
Madeleine Nkiru <anaghamadeleine@gmail.com>
2026-03-14 16:48:35 -0400

    exercise 3 completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -8,12 +8,15 @@ class Program
         int magicNumber = generateRandomly.Next(1, 101);
 
         int guess = -1;
+        int guessCounter = 0;
 
         while (guess != magicNumber)
         {
             Console.Write("What is your preferred guess? ");
             string userInput = Console.ReadLine();
             guess = int.Parse(userInput);
+
+            guessCounter++;
 
             if (guess < magicNumber)
             {
@@ -26,6 +29,7 @@ class Program
             else
             {
                 Console.WriteLine("You guessed it right!");
+                Console.WriteLine($"It took you {guessCounter} guesses to get to the magic number!");
             }
         }
     }
