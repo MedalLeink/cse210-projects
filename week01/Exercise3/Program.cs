e3c4ad21dfe85d9115b4d2cc827571b949d43fdf
Madeleine Nkiru <anaghamadeleine@gmail.com>
2026-03-14 16:37:32 -0400

    exercise 3 completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random generateRandomly = new Random();
+        int magicNumber = generateRandomly.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your preferred guess? ");
+            string userInput = Console.ReadLine();
+            guess = int.Parse(userInput);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it right!");
+            }
+        }
     }
 }
